18b4e7c63b1bb9337776bd8c1c3add04450035eb
Loller208 <108628779+Loller208@users.noreply.github.com>
2024-07-25 11:51:47 +0200

    Delete fileSaver.js

--- fileSaver.js
+++
@@ -1,9 +0,0 @@
-// fileSaver.js
-const fs = require('fs');
-
-function saveFile(filename, content) {
-  fs.writeFileSync(filename, content, 'utf8');
-  console.log(`File saved: ${filename}`);
-}
-
-module.exports = saveFile;
